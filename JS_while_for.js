44c317312c6ff64550bda347ba97d5755968a115
Nataly <nlesniak1987@gmail.com>
2022-06-29 23:55:53 +0300

    lesson 2 JS

---
+++ JS_while_for.js
@@ -0,0 +1,38 @@
+// let age = 18
+// let money = 900
+// let dog = false
+// let dog_show = true
+
+// if (age >= 18 && money == 1000 && dog){
+//     console.log("Get ticket")
+// } else {if (age < 18) {
+//     console.log("Early")
+// } if (money < 1000) {
+//     console.log("Keep calm and work hard!!")
+// } if (!dog) {
+//     console.log("Get dog")
+// } }
+
+// switch (dog_show) {
+//     case age >=18 :
+//         console.log("Time to win")
+//         // break;
+//     case money >= 1000 :
+//         console.log("Rich")
+//         break;        
+// }
+
+count = 0
+// while (count < 10){
+//     // console.log(count, "Win!")
+//     if (count % 2 == 0){
+//     console.log(count, "Even")
+// }
+//     count ++
+// }
+mm = ["Alla", "Nataly", "Tom", "Kate"]
+    //console.log(mm.lenght)
+
+    for (let i =0; i < mm.lenght; i++){
+    console.log(i, mm[i], "Win!")
+}
